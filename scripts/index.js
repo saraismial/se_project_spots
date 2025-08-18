72be1a0eb0ca4d7995665a116ec6037f53a2b0e8
Sara Ismial <sara-ismial@outlook.com>
2025-08-18 16:26:28 -0400

    stage 6 form submission

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,3 @@
-const modalEditProfile = document.querySelector(".modal_edit-profile");
-const editProfileButton = document.querySelector(".profile__edit-btn");
-const modalPostProfile = document.querySelector(".modal_post-profile");
-const newPostButton = document.querySelector(".profile__post-btn");
-const modalEditCloseButton = modalEditProfile.querySelector(".modal__close-btn");
-const modalPostCloseButton = modalPostProfile.querySelector(".modal__close-btn");
-
-
-editProfileButton.addEventListener("click", function() {
-  modalEditProfile.classList.add("modal_is-opened");
-});
-
-modalEditCloseButton.addEventListener("click", function() {
-  modalEditProfile.classList.remove("modal_is-opened");
-});
-
-newPostButton.addEventListener("click", function() {
-  modalPostProfile.classList.add("modal_is-opened");
-});
-
-modalPostCloseButton.addEventListener("click", function() {
-  modalPostProfile.classList.remove("modal_is-opened");
-});
-
-
 const initialCards = [
   {
     name: "Val Thorens",
@@ -48,8 +23,81 @@ const initialCards = [
     name: "Mountain house",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"
   }
-]
+];
+const modalEditProfile = document.querySelector(".modal_edit-profile");
+const editProfileButton = document.querySelector(".profile__edit-btn");
+const modalEditCloseButton = modalEditProfile.querySelector(".modal__close-btn");
+const editProfileFormElement = modalEditProfile.querySelector(".modal__form");
+const modalEditProfileNameInput = modalEditProfile.querySelector("#profile-name-input");
+const modalEditProfileDescriptionInput = modalEditProfile.querySelector("#profile-description-input");
+
+const profileNameElement = document.querySelector(".profile__name");
+const profileDescriptionElement = document.querySelector(".profile__description");
+
+const modalPostProfile = document.querySelector(".modal_post-profile");
+const newPostButton = document.querySelector(".profile__post-btn");
+const modalPostCloseButton = modalPostProfile.querySelector(".modal__close-btn");
+const postProfileFormElement = modalPostProfile.querySelector(".modal__form");
+const modalPostProfileLinkInput = modalPostProfile.querySelector("#profile-image-link");
+const modalPostProfileCaptionInput = modalPostProfile.querySelector("#profile-image-caption");
+
+const cardLikeButtons = document.querySelectorAll(".card__like-btn");
+const cardLikedButtons = document.querySelectorAll(".card__liked-btn");
+
+
+editProfileButton.addEventListener("click", function() {
+  modalEditProfile.classList.add("modal_is-opened");
+  modalEditProfileNameInput.value = profileNameElement.textContent;
+  modalEditProfileDescriptionInput.value = profileDescriptionElement.textContent;
+});
+
+modalEditCloseButton.addEventListener("click", function() {
+  modalEditProfile.classList.remove("modal_is-opened");
+});
+
+newPostButton.addEventListener("click", function() {
+  modalPostProfile.classList.add("modal_is-opened");
+});
+
+modalPostCloseButton.addEventListener("click", function() {
+  modalPostProfile.classList.remove("modal_is-opened");
+});
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  const modalEditProfileNameInputValue = modalEditProfileNameInput.value;
+  const modalEditProfileDescriptionInputValue = modalEditProfileDescriptionInput.value;
+
+  profileNameElement.textContent = modalEditProfileNameInputValue;
+  profileDescriptionElement.textContent = modalEditProfileDescriptionInputValue;
+
+  modalEditProfile.classList.remove("modal_is-opened");
+}
+
+editProfileFormElement.addEventListener('submit', handleProfileFormSubmit);
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+
+  const modalPostProfileLinkInputValue = modalPostProfileLinkInput.value;
+  const modalPostProfileCaptionInputValue = modalPostProfileCaptionInput.value;
+
+  console.log(`Image Link: ${modalPostProfileLinkInputValue}, Caption: ${modalPostProfileCaptionInputValue}`);
+
+  modalPostProfile.classList.remove("modal_is-opened");
+}
+
+postProfileFormElement.addEventListener('submit', handleAddCardSubmit);
 
 initialCards.forEach(function(item) {
   console.log(`Name: ${item.name}, Link: ${item.link}`);
 });
+
+cardLikeButtons.forEach(function(likeButton, index) {
+  likeButton.addEventListener("click", function() {
+    cardLikedButtons[index].classList.add("card__liked-btn_visible");
+    likeButton.classList.add("card__like-btn_visible");
+  });
+});
+
